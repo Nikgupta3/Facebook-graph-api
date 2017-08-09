dd07a23cdbdbe9ecfe4127b99c2237d26953454e
Nikgupta3 <nik_gupta3@live.com>
2017-08-09 10:33:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,232 @@
+var fbSearch = {};
+
+(function (fbSearch) {
+    "use strict";
+    fbSearch = function () {
+
+        /* Private Variable Declaration */
+        var $accessToken = "422844521144379|tep2pW6z17sY4I4FGjJQW3h6Y3Q",
+            $baseUrl = "https://graph.facebook.com/",
+            $btnSearch = document.getElementById("btnSearch"),
+            $txtSearch = document.getElementById("txtSearch"),
+            $searchResultContainer = document.getElementById("search-result-container"),
+            $placeholderImg = "http://placehold.it/50/55C1E7/fff",
+            $nolink = 'javascript:void(0)',
+            $filteredList = [];
+
+        var $searchResult = [];
+
+        function init() {
+
+            // Event Listener
+            addEvent($btnSearch, 'click', search_click);
+           
+        };
+
+        // Common method to trigger AJAX calls when required.
+        function ajax(url, callback, type, async) {
+            async = async || true;
+            type = type || "GET";
+
+            var xmlhttp;
+            if (window.XMLHttpRequest) {
+                // IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                // IE7-
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4) {
+                    if (xmlhttp.status == 200) {
+                        if (xmlhttp.responseText) {
+                            callback(JSON.parse(xmlhttp.responseText));
+                        }
+                        else {
+                            console.log('no records found');
+                        }
+                    }
+                    else {
+                        alert('Error occured. Please try again')
+                    }
+                }
+            };
+            xmlhttp.onerror = function (err) {
+                console.log(err);
+            };
+            xmlhttp.open(type, url, async);
+            xmlhttp.send();
+        };
+
+        // Functionality to Search Facebook Pages
+        function searchPages(searchstring) {
+            clearResult();
+            ajax($baseUrl + "search?q=" + searchstring + "&type=page&access_token=" + $accessToken + "&fields=id,name,picture,about,description,category,link,likes", receivePageList)
+        };
+
+        // Action performed when FB returns the object with Page Details
+        function receivePageList(data) {
+           //f $sortContainer.style.display = 'block';
+            $searchResult = data.data;
+            
+            renderSearchResult($searchResult);
+        };
+
+        // Generic function to add event to DOM element for both primitive and new browsers.
+        function addEvent(element, evnt, funct) {
+            if (element.attachEvent)
+                return element.attachEvent('on' + evnt, funct);
+            else
+                return element.addEventListener(evnt, funct, false);
+        }
+
+        // Click event for Search button.
+        function search_click(e) {
+            e = e || window.event;
+            e.preventDefault();
+            e.stopPropagation();
+
+            var valid = validate($txtSearch.value);
+
+            if (valid) searchPages($txtSearch.value);
+        };
+
+        // Small functionality to check if textbox is empty.
+        function validate(text) {
+            if (text === '') {
+                alert("Please enter valid search query");
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        // Creating the search result HTML markup using vanilla JS.
+        function renderSearchResult(data) {
+            var $ul = document.getElementsByClassName("search-result")[0];
+            var noOfItemsInRow = 4;
+
+            if (data.length > 0) {
+                var counter = 1;
+                var $row = document.createElement('div');
+                for (var i = 0; i < data.length; i++) {
+                    var $hidden = document.createElement('input');
+                    var $col = document.createElement('div'),
+                        $panel = document.createElement('div');
+
+                    $hidden.type = 'hidden';
+                    $hidden.value = data[i].id;
+                    $row.className = 'row';
+                    $col.className = 'col-lg-' + noOfItemsInRow;
+                    $panel.className = 'panel widget';
+
+                    // Image / First Div
+                    var $d1 = document.createElement('div'),
+                        $d1Img = document.createElement('img'),
+                        $d1div = document.createElement('div'),
+                            $d1divImg = document.createElement('img');
+
+                    $d1.className = 'half-float';
+                    $d1Img.src = '';
+                    $d1Img.className = 'img-responsive';
+
+                    $d1div.className = 'half-float-bottom';
+                    $d1divImg.src = data[i].picture.data.url || $placeholderImg;
+                    $d1divImg.className = 'img-thumbnail img-circle thumb128';
+
+                    $d1div.appendChild($d1divImg);
+                    $d1.appendChild($d1Img);
+                    $d1.appendChild($d1div);
+                    $panel.appendChild($d1);
+
+                    // Name and Category
+                    var $d2 = document.createElement('div'),
+                        $d2h3 = document.createElement('h3'),
+                        $d2h3a = document.createElement('a'),
+                        $d2p = document.createElement('p'),
+                        $d2p2 = document.createElement('p');
+
+                    $d2.className = 'panel-body text-center';
+                    $d2h3.className = 'm0';
+                    $d2h3a.href = data[i].link || $nolink;
+                    $d2h3a.innerText = data[i].name;
+                    $d2h3.appendChild($d2h3a);
+                    $d2.appendChild($d2h3);
+
+                    $d2p.className = 'text-muted';
+                    $d2p.innerText = data[i].category || 'No Category Found';
+                    $d2.appendChild($d2p);
+
+                    $d2p2.innerHTML = data[i].description || 'No Description Found';
+                    $d2.appendChild($d2p2);
+                    $panel.appendChild($d2);
+
+                    // Description and Like count.
+                    var d3 = document.createElement('div'),
+                        d3d1 = document.createElement('div'),
+                        d3d1d1 = document.createElement('div'),
+                        d3d1d2 = document.createElement('div'),
+                        $span = document.createElement('span'),
+                        d3d2 = document.createElement('div'),
+                        d3d2h = document.createElement('h3'),
+                        d3d2p = document.createElement('p');
+
+                    d3.className = 'panel-body text-center bg-gray-dark';
+                    d3d1.className = 'panel-widget-footer';
+                    d3d1d1.className = 'col-xs-4 pull-left';
+                   
+                    d3d2.className = 'col-xs-4 pull-right';
+                    d3d2h.className = 'm0';
+                    d3d2p.className = 'm0';
+                    d3d2p.innerText = 'Likes';
+                    d3d2h.innerText = data[i].likes;
+
+                    d3d1d1.appendChild($span);
+                    d3d1.appendChild(d3d1d1);
+                    d3d2.appendChild(d3d2h);
+                    d3d2.appendChild(d3d2p);
+
+                    d3.appendChild(d3d1);
+                    d3.appendChild(d3d2);
+
+                    $panel.appendChild(d3);
+
+                    $col.appendChild($hidden);
+                    $col.appendChild($panel);
+
+                    $row.appendChild($col);
+                }
+                $ul.appendChild($row);
+            }
+            else {
+
+                // When no data - add empty div to display custom message
+                var $liEmpty = document.createElement('li');
+                $liEmpty.innerHTML = "<div class='alert alert-danger'>" +
+                    "No data found matching your search query." +
+                    "</div>";
+                $liEmpty.className = "no-data";
+                $ul.appendChild($liEmpty);
+            }
+        };
+
+        // Clear the search result before appending data to search result.
+        function clearResult() {
+            document.getElementsByClassName("search-result")[0].innerHTML = "";
+        };
+
+        
+        
+
+       
+
+        return {
+            /* Public Properties*/
+            init: init
+        };
+    };
+
+    fbSearch().init();
+
+})(fbSearch);
